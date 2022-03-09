6698c8b07dffb1502ebf9ec1bd9b20eecfaf7a6d
Esther Carminatti <yunalesca002@gmail.com>
2022-03-09 15:20:19 +0000

    primary_tag fixed

--- db/migrate/20220308191731_add_primary_tag_to_influencer.rb
+++ db/migrate/20220308191731_add_primary_tag_to_influencer.rb
@@ -1,5 +1,6 @@
 class AddPrimaryTagToInfluencer < ActiveRecord::Migration[6.0]
   def change
-    add_reference :influencers, :primary_tag, null: false, foreign_key: { to_table: 'tags' }
+    add_reference :influencers, :primary_tag, null: false
+    add_foreign_key :influencers, :tags, column: :primary_tag_id, primary_key: :id
   end
 end
